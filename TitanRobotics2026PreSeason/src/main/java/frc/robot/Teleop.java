5e52cd33694c6d0b39857f8554dc429dc7a908c9
FRC8334Students <frc8334student@gmail.com>
2025-11-04 17:53:02 -0700

    first version to drive. alignment is all off, some issue with the encoders is preventing proper driving

--- TitanRobotics2026PreSeason/src/main/java/frc/robot/Teleop.java
+++ TitanRobotics2026PreSeason/src/main/java/frc/robot/Teleop.java
@@ -10,35 +10,35 @@ import frc.robot.Data.PortMap;
 
 public class Teleop {
 
-    Controller driverController;
-    SwerveBase swerveBase;
+    Controller driverController; //object of Controller
+    SwerveBase swerveBase; //object of SwerveBase
 
-    private double controllerLeftX;
-    private double controllerLeftY;
-    private double controllerRightX;
-    private boolean controllerRightBumper;
+    private double controllerLeftX; //variable for the left x joystick axis
+    private double controllerLeftY; //variable for the left y joystick axis
+    private double controllerRightX; //variable for the right x joystick axis
+    private boolean controllerRightBumper; //variable for if the right bumper is pressed
 
     public Teleop() {
-        driverController = new Controller(PortMap.DRIVER_CONTROLLER);
-        swerveBase = SwerveBase.getInstance();
+        driverController = new Controller(PortMap.DRIVER_CONTROLLER); //creates a new controller
+        swerveBase = SwerveBase.getInstance(); //gets an instance of SwerveBase
     }
 
-    public void teleopPeriodic()
+    public void teleopPeriodic() //everything in this method will get executed 
     {
-        driveBaseControl();
+        driveBaseControl(); //executes the driveBaseControl method
     }
 
     public void driveBaseControl() {
-        controllerLeftY = driverController.getLeftY();
-        controllerLeftX = driverController.getLeftX();
-        controllerRightX = driverController.getRightX();
-        controllerRightBumper = driverController.getRightBumperButton();
+        controllerLeftY = driverController.getLeftY(); //sets the variable controllerLeftY to the actual data coming from the controller
+        controllerLeftX = driverController.getLeftX(); //sets the variable controllerLeftX to the actual data coming from the controller
+        controllerRightX = driverController.getRightX(); //sets the variable controllerRightX to the actual data coming from the controller
+        controllerRightBumper = driverController.getRightBumperButton(); //sets the variable controllerRightBumper to the actual data coming from the controller
 
-        double forward;
-        double strafe;
-        double rotation;
+        double forward; 
+        double strafe; //Rhea this means going side to side
+        double rotation = 0;
 
-        boolean isFieldOrriented = true;
+        boolean isFieldOrriented = false;
 
         if (Math.abs(controllerLeftY) >= 0.1) {
             forward = (controllerLeftY);
@@ -50,11 +50,11 @@ public class Teleop {
         } else {
             strafe = 0;
         }
-        if (Math.abs(controllerRightX) >= 0.1) {
+        /*if (Math.abs(controllerRightX) >= 0.1) {
             rotation = (controllerRightX);
         } else {
             rotation = 0;
-        }
+        }*/
 
         if(controllerRightBumper && isFieldOrriented == true)
         {
@@ -66,7 +66,7 @@ public class Teleop {
             isFieldOrriented = true;
         }
 
-        if(isFieldOrriented)
+        if(isFieldOrriented) //translation2d is used for lateral movement of the swerve drive
         {
             swerveBase.drive(new Translation2d(forward,strafe), rotation, true);
         }
